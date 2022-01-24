5313fa9d19bac63a76c0f6f5c4ab0883975eaf59
oli-fire <87943760+oli-fire@users.noreply.github.com>
2022-01-24 20:12:44 +0000

    Add files via upload

---
+++ characters_elements.js
@@ -0,0 +1,17 @@
+// Luke Skywalker
+document.getElementById("p_a_name").innerHTML = p.a.name;
+document.getElementById("p_a_homeworld").innerHTML = p.a.homeworld;
+document.getElementById("p_a_born").innerHTML = p.a.born;
+document.getElementById("p_a_died").innerHTML = p.a.died;
+document.getElementById("p_a_species").innerHTML = p.a.species;
+document.getElementById("p_a_gender").innerHTML = p.a.gender;
+document.getElementById("p_a_height").innerHTML = p.a.height;
+document.getElementById("p_a_mass").innerHTML = p.a.mass;
+document.getElementById("p_a_haircolor").innerHTML = p.a.haircolor;
+document.getElementById("p_a_eyecolor").innerHTML = p.a.eyecolor;
+document.getElementById("p_a_skincolor").innerHTML = p.a.skincolor;
+document.getElementById("p_a_cybernetics").innerHTML = p.a.cybernetics;
+document.getElementById("p_a_affiliations").innerHTML = p.a.affiliations;
+document.getElementById("p_a_apprentices").innerHTML = p.a.apprentices;
+document.getElementById("p_a_weapons").innerHTML = p.a.weapons;
+document.getElementById("p_a_des").innerHTML = p.a.des;
