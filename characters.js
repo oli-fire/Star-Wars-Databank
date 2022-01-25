6690a8725c84e00d47703126ceb2f6d66bb18aee
oli-fire <87943760+oli-fire@users.noreply.github.com>
2022-01-25 13:39:00 +0000

    Update characters.js

--- characters.js
+++ characters.js
@@ -14,9 +14,9 @@ a : {
   eyecolor : 'Blue',
   skincolor : 'Light',
   cybernetics : "Prosthetic right hand",
-  affiliations : ['Rebelion', 'Jedi Order', 'New Republic'],
-  masters : ["Obi-Wan Kenobi", 'Yoda'],
-  apprentices : ['Leia Organa', 'Grogu', 'Ben Solo', 'Rey'],
+  affiliations : ['Rebelion', ' Jedi Order', ' New Republic'],
+  masters : ["Obi-Wan Kenobi", ' Yoda'],
+  apprentices : ['Leia Organa', ' Grogu', ' Ben Solo', ' Rey'],
   weapons : 'Lightsaber',
   des : "Luke Skywalker, a Force-sensitive human male, was a legendary Jedi Master who fought in the Galactic Civil War during the reign of the Galactic Empire. Along with his companions, Princess Leia Organa and General Han Solo, Skywalker served as a revolutionary on the side of the Alliance to Restore the Republic—an organization committed to the downfall of the Galactic Empire and the restoration of democracy. Following the war, Skywalker became a living legend, and was remembered as one of the greatest Jedi in galactic history."
 }
