4c1ebab05df53ae9026b6ce4b160d3daa014857d
oli-fire <87943760+oli-fire@users.noreply.github.com>
2022-01-25 17:15:30 +0000

    Update characters.js

--- characters.js
+++ characters.js
@@ -43,3 +43,39 @@ b : {
 }
 }
 
+// Luke Skywalker
+document.getElementById("p_a_img").src = p.a.image;
+document.getElementById("p_a_name").innerHTML = p.a.name;
+document.getElementById("p_a_homeworld").innerHTML = p.a.homeworld;
+document.getElementById("p_a_born").innerHTML = p.a.born;
+document.getElementById("p_a_died").innerHTML = p.a.died;
+document.getElementById("p_a_species").innerHTML = p.a.species;
+document.getElementById("p_a_gender").innerHTML = p.a.gender;
+document.getElementById("p_a_height").innerHTML = p.a.height;
+document.getElementById("p_a_mass").innerHTML = p.a.mass;
+document.getElementById("p_a_haircolor").innerHTML = p.a.haircolor;
+document.getElementById("p_a_eyecolor").innerHTML = p.a.eyecolor;
+document.getElementById("p_a_skincolor").innerHTML = p.a.skincolor;
+document.getElementById("p_a_cybernetics").innerHTML = p.a.cybernetics;
+document.getElementById("p_a_affiliations").innerHTML = p.a.affiliations;
+document.getElementById("p_a_apprentices").innerHTML = p.a.apprentices;
+document.getElementById("p_a_weapons").innerHTML = p.a.weapons;
+document.getElementById("p_a_des").innerHTML = p.a.des;
+// Leia Skywalker Organa Solo
+document.getElementById("p_b_img").src = p.b.image;
+document.getElementById("p_b_name").innerHTML = p.b.name;
+document.getElementById("p_b_homeworld").innerHTML = p.b.homeworld;
+document.getElementById("p_b_born").innerHTML = p.b.born;
+document.getElementById("p_b_died").innerHTML = p.b.died;
+document.getElementById("p_b_species").innerHTML = p.b.species;
+document.getElementById("p_b_gender").innerHTML = p.b.gender;
+document.getElementById("p_b_height").innerHTML = p.b.height;
+document.getElementById("p_b_mass").innerHTML = p.b.mass;
+document.getElementById("p_b_haircolor").innerHTML = p.b.haircolor;
+document.getElementById("p_b_eyecolor").innerHTML = p.b.eyecolor;
+document.getElementById("p_b_skincolor").innerHTML = p.b.skincolor;
+document.getElementById("p_b_cybernetics").innerHTML = p.b.cybernetics;
+document.getElementById("p_b_affiliations").innerHTML = p.b.affiliations;
+document.getElementById("p_b_apprentices").innerHTML = p.b.apprentices;
+document.getElementById("p_b_weapons").innerHTML = p.b.weapons;
+document.getElementById("p_b_des").innerHTML = p.b.des;
